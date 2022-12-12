0ccac79649346bd4458976b6c54432a1f71f5e43
mjcheng <59213556+minjiecheng@users.noreply.github.com>
2022-12-12 20:49:52 +0800

    updata rotpooling

--- backbones/resnet-imagenet.py
+++ backbones/resnet-imagenet.py
@@ -62,7 +62,7 @@ parser.add_argument('--world-size', default=1, type=int,
                     help='number of nodes for distributed training')
 parser.add_argument('--rank', default=0, type=int,
                     help='node rank for distributed training')
-parser.add_argument('--dist-url', default='tcp://', type=str,
+parser.add_argument('--dist-url', default='tcp:// ', type=str,
                     help='url used to set up distributed training')
 parser.add_argument('--dist-backend', default='nccl', type=str,
                     help='distributed backend')
